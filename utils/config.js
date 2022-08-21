dd9fe81710c2c8367e154e6df964870f7482cc9e
Munirudeen Ibrahim <munircole@gmail.com>
2022-08-21 17:14:30 +0100

    added procfile for heroku

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-let PORT = '5000';
+let PORT = process.env.PORT|| 5000;
 let MONGODB_URI = "mongodb+srv://munir:yaxvCGNN8NsXlGmW@cluster0.asqpf.mongodb.net/BMate?retryWrites=true&w=majority";
 let SECRET = "##MStar200";
 
